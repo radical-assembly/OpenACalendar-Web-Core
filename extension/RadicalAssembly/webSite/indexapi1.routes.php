9247c0260c7dbda134a26570fb87a47a327ff805
Elias <elias0789@gmail.com>
2015-11-30 22:51:23 +0000

    Add new api1 route to get back an events object compatible with FullCalendar.js

--- extension/RadicalAssembly/webSite/indexapi1.routes.php
+++ extension/RadicalAssembly/webSite/indexapi1.routes.php
@@ -8,11 +8,11 @@
  * @author Elias Malik <elias0789@gmail.com>
  */
 
-
-$app->match('/api1/radicalassembly/list/{slug}/events.ical', "org\openacalendar\\radicalassembly\siteapi1\controllers\EventListController::ical")
+$app->match('/api1/radicalassembly/list/{slug}/events.fullcalendar', "org\openacalendar\\radicalassembly\siteapi1\controllers\EventListController::fullcalendar")
 		->assert('slug', FRIENDLY_SLUG_REGEX);
-
 $app->match('/api1/radicalassembly/list/{slug}/events.json', "org\openacalendar\\radicalassembly\siteapi1\controllers\EventListController::json")
  		->assert('slug', FRIENDLY_SLUG_REGEX);
+$app->match('/api1/radicalassembly/list/{slug}/events.ical', "org\openacalendar\\radicalassembly\siteapi1\controllers\EventListController::ical")
+		->assert('slug', FRIENDLY_SLUG_REGEX);
 
 $app->match('/api1/radicalassembly/venues.json', "org\openacalendar\\radicalassembly\siteapi1\controllers\VenueListController::listJson");
